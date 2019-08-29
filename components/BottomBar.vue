955cc160efebf7122263930ddfdf481e5d7e66e0
joerverson.santos <joerverson.santos@gmail.com>
2019-08-29 17:14:27 -0300

    init

---
+++ components/BottomBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="absolute inset-x-0 bottom-0 h-20 bar-color flex flex-row">
+    <div class="w-24 h-24 rounded-full ml-8 -mt-8 border-gray-800 border-4">
+      <img
+        class="object-cover rounded-full"
+        src="https://avatars2.githubusercontent.com/u/7293460?s=460&v=4"
+        alt
+      />
+    </div>
+    <div class="flex justify-center text-white py-4">
+      <a href='#' class='px-20'>Projetos</a>
+      <a href='diagrams' class='px-20'>Fluxs</a>
+      <a href='#' class='px-20'>Propostas</a>
+    </div>
+  </div>
+</template>
+
+<style lang="css">
+.border-gray-800 {
+  border-color: #373d42;
+}
+</style>
---
+++ pages/Diagrams.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class='container'>
+    <iframe src="https://demo.bpmn.io/new" class='w-screen' style="height: 80vh" frameborder="0"></iframe>
+  </div>
+</template>
+
+<script>
+import BpmnJS from 'bpmn-js';
+
+export default {
+  mounted: () => {},
+  methods: {
+  }  
+}
+</script>
